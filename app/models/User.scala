4feec2adbfe976488eead593ed577357e5c5d342
hiforujitaaki <hirofujitaaki.gmail.com>
2017-09-02 11:48:05 +0900

    implement: models/User.scala

---
+++ app/models/User.scala
@@ -0,0 +1,35 @@
+package models
+
+import java.util.UUID
+
+import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
+
+/**
+ * The user object.
+ * @param loginInfo The linked login info.
+ * @param avatarURL Maybe the avatar URL of the authenticated provider.
+ * @param activated Indicates that the user has activated its registration.
+ */
+case class User(
+  userID: UUID,
+  loginInfo: LoginInfo,
+  firstName: Option[String],
+  lastName: Option[String],
+  fullName: Option[String],
+  email: Option[String],
+  avatarURL: Option[String],
+  activated: Boolean) extends Identity {
+
+  /**
+   * Tries to construct a name.
+   * @return Maybe a name.
+   */
+  def name = fullName.orElse {
+    firstName -> lastName match {
+      case (Some(f), Some(l)) => Some(f + " " + l)
+      case (Some(f), None) => Some(f)
+      case (None, Some(l)) => Some(l)
+      case _ => None
+    }
+  }
+}
