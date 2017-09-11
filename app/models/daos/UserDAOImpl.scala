3df8817ecc3a6e666a8685c31852692759de3e4c
hiforujitaaki <hirofujitaaki.gmail.com>
2017-09-11 16:06:00 +0900

    fix: daos/UserDAO and the Impl

--- app/models/daos/UserDAOImpl.scala
+++ app/models/daos/UserDAOImpl.scala
@@ -3,10 +3,10 @@ package models.daos
 import java.util.UUID
 import javax.inject.Inject
 
-import models.AuthToken
-import models.daos.AuthTokenDAOImpl._
-import models.tables.{ AuthTokenTable, DbAuthToken }
-import org.joda.time.DateTime
+import com.mohiva.play.silhouette.api.LoginInfo
+import models.User
+import models.daos.UserDAOImpl._
+import models.tables._
 import play.api.db.slick.DatabaseConfigProvider
 import slick.backend.DatabaseConfig
 import slick.driver.JdbcProfile
@@ -17,62 +17,112 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 /**
- * Give access to the [[AuthToken]] object.
+ * Give access to the user object.
  */
-class AuthTokenDAOImpl @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends AuthTokenDAO {
+class UserDAOImpl @Inject() (protected val dbConfigProvider: DatabaseConfigProvider) extends UserDAO {
 
   val dbConfig: DatabaseConfig[JdbcProfile] = dbConfigProvider.get[JdbcProfile]
   val db: JdbcBackend#DatabaseDef = dbConfig.db
 
   import dbConfig.driver.api._
 
+  def loginInfoQuery(loginInfo: LoginInfo): Query[LoginInfoTable, DbLoginInfo, Seq] = {
+    loginInfos.filter(dbLoginInfo => dbLoginInfo.providerId === loginInfo.providerID && dbLoginInfo.providerKey === loginInfo.providerKey)
+  }
+
   /**
-   * Finds a token by its ID.
+   * Finds a user by its login info.
    *
-   * @param id The unique token ID.
-   * @return The found token or None if no token for the given ID could be found.
+   * @param loginInfo The login info of the user to find.
+   * @return The found user or None if no user for the given login info could be found.
    */
-  def find(id: UUID): Future[Option[AuthToken]] = {
-    db.run(tokens.filter(_.id === id.toString).result.headOption).map { resultOption =>
-      resultOption.map {
-        dbToken => AuthToken(UUID.fromString(dbToken.id), UUID.fromString(dbToken.userID), DateTime.parse(dbToken.expiry))
+  def find(loginInfo: LoginInfo): Future[Option[User]] = {
+    val userQuery = for {
+      dbLoginInfo <- loginInfoQuery(loginInfo)
+      dbUserLoginInfo <- userLoginInfos.filter(_.loginInfoId === dbLoginInfo.id)
+      dbUser <- users.filter(_.userID === dbUserLoginInfo.userId)
+    } yield dbUser
+
+    db.run(userQuery.result.headOption).map { dbUserOption =>
+      dbUserOption.map {
+        user => User(UUID.fromString(user.userID), loginInfo, user.firstName, user.lastName, user.fullName, user.email, user.avatarUrl, user.activated)
       }
     }
   }
 
   /**
-   * Saves a token.
+   * Finds a user by its user ID.
    *
-   * @param token The token to save.
-   * @return The saved token.
+   * @param userID The ID of the user to find.
+   * @return The found user or None if no user for the given ID could be found.
    */
-  def save(token: AuthToken): Future[AuthToken] = {
-    db.run((
-      tokens returning tokens.map(_.id) into ((token, id) => token.copy(id))
-    ) += DbAuthToken(token.id.toString, token.userID.toString, token.expiry.toString)).map { _ =>
-      token
+  def find(userID: UUID): Future[Option[User]] = {
+    {
+      val userQuery = for {
+        dbUser <- users.filter(_.userID === userID.toString)
+        dbUserLoginInfo <- userLoginInfos.filter(_.userId === dbUser.userID)
+        dbLoginInfo <- loginInfos.filter(_.id === dbUserLoginInfo.loginInfoId)
+      } yield (dbUser, dbLoginInfo)
+
+      db.run(userQuery.result.headOption).map { resultOption =>
+        resultOption.map {
+          case (user, loginInfo) => User(
+            UUID.fromString(user.userID),
+            LoginInfo(loginInfo.providerId, loginInfo.providerKey),
+            user.firstName,
+            user.lastName,
+            user.fullName,
+            user.email,
+            user.avatarUrl,
+            user.activated
+          )
+        }
+      }
     }
   }
 
   /**
-   * Removes the token for the given ID.
+   * Saves a user.
    *
-   * @param id The ID for which the token should be removed.
-   * @return A future to wait for the process to be completed.
+   * @param user The user to save.
+   * @return The saved user.
    */
-  def remove(id: UUID): Future[Int] = {
-    db.run(tokens.filter(_.id === id.toString).delete)
+  def save(user: User): Future[User] = {
+    val dbUser = DbUser(user.userID.toString, user.firstName, user.lastName, user.fullName, user.email, user.avatarURL, user.activated)
+    val dbLoginInfo = DbLoginInfo(None, user.loginInfo.providerID, user.loginInfo.providerKey)
+
+    val loginInfoAction = {
+      val retrieveLoginInfo = loginInfos.filter(
+        info => info.providerId === user.loginInfo.providerID &&
+          info.providerKey === user.loginInfo.providerKey
+      ).result.headOption
+
+      val insertLoginInfo = loginInfos.returning(loginInfos.map(_.id)).
+        into((info, id) => info.copy(id)) += dbLoginInfo
+
+      for {
+        loginInfoOption <- retrieveLoginInfo
+        loginInfo <- loginInfoOption.map(DBIO.successful).getOrElse(insertLoginInfo)
+      } yield loginInfo
+    }
+
+    val actions = (for {
+      _ <- users.insertOrUpdate(dbUser)
+      loginInfo <- loginInfoAction
+      _ <- userLoginInfos += DbUserLoginInfo(dbUser.userID, loginInfo.id.get)
+    } yield ()).transactionally
+
+    db.run(actions).map(_ => user)
   }
 }
 
 /**
  * The companion object.
  */
-object AuthTokenDAOImpl {
+object UserDAOImpl {
 
-  /**
-   * The list of tokens.
-   */
-  private val tokens = TableQuery[AuthTokenTable]
+  private val loginInfos = TableQuery[LoginInfoTable]
+  private val users = TableQuery[UserTable]
+  private var userLoginInfos = TableQuery[UserLoginInfoTable]
 
 }
