c0ffcd1c96754f3ebff1d8df86985921d0245ee6
hiforujitaaki <hirofujitaaki.gmail.com>
2017-10-09 16:01:39 +0900

    implement: time elapsed

---
+++ app/views/Helpers.scala
@@ -0,0 +1,33 @@
+package views
+
+import org.joda.time.{ DateTime, Period }
+import org.joda.time.format.PeriodFormatter
+import org.joda.time.format.PeriodFormatterBuilder
+
+object Helpers {
+
+  def timeElapsed(timePosted: DateTime) = {
+    val now = new DateTime()
+    val period = new Period(timePosted, now)
+
+    val formatter: PeriodFormatter = {
+      if (period.getYears() != 0)
+        new PeriodFormatterBuilder().appendYears().appendSuffix(" years ago\n").printZeroNever().toFormatter()
+      else if (period.getMonths() != 0)
+        new PeriodFormatterBuilder().appendMonths().appendSuffix(" months ago\n").printZeroNever().toFormatter()
+      else if (period.getWeeks() != 0)
+        new PeriodFormatterBuilder().appendWeeks().appendSuffix(" weeks ago\n").printZeroNever().toFormatter()
+      else if (period.getDays() != 0)
+        new PeriodFormatterBuilder().appendDays().appendSuffix(" days ago\n").printZeroNever().toFormatter()
+      else if (period.getHours() != 0)
+        new PeriodFormatterBuilder().appendHours().appendSuffix(" hours ago\n").printZeroNever().toFormatter()
+      else if (period.getMinutes() != 0)
+        new PeriodFormatterBuilder().appendMinutes().appendSuffix(" minutes ago\n").printZeroNever().toFormatter()
+      else
+        new PeriodFormatterBuilder().appendSeconds().appendSuffix(" seconds ago\n").printZeroNever().toFormatter()
+    }
+
+    formatter.print(period)
+  }
+}
+
